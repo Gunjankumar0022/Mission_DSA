e2d0ff3544cd5af597b7eaac4b950b5ddbfbe991
Gunjan Kumar <gunjankumar0022@gmail.com>
2022-06-29 20:10:59 +0530

    Pyramid.c Added

---
+++ Pyramid.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int i,j,k;
+    for(i=1;i<=10;i++)
+    {
+        for(k=10;k>i;k--)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return(0);
+
+}
