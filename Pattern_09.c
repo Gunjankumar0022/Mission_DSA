d4e3d9ccdf2aaf874db81d285ad28262668ea9d4
Gunjan Kumar <gunjankumar0022@gmail.com>
2022-07-05 23:01:26 +0530

    Pattern_09.c Updated

--- Pattern_09.c
+++ Pattern_09.c
@@ -4,9 +4,9 @@ int main()
    int i,j,k=0;
    for(i=1;i<=5;i++)
    {
-    for(j=i;j>=i;j--)
+    for(j=i;j<=i*5;j++)
     {
-        k = k + i;
+        k = k + 1;
         printf("%d\t",k);
     }
     printf("\n");
