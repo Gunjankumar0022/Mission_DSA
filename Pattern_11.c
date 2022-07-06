e64268f64e1e65e914b41d3cb26df18565b6ed70
Gunjan Kumar <gunjankumar0022@gmail.com>
2022-07-07 00:57:50 +0530

    Pattern_11.c Added

---
+++ Pattern_11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,k,m,n;
+    for(i=1;i<=5;i++)
+    {
+        for(j=5;j>=i;j--)
+        {
+            printf("%d\t",j);
+        }
+        for(k=1;k<i;k++)
+        {
+            printf("*\t");
+        }
+        for(m=1;m<i;m++)
+        {
+            printf("*\t");
+        }
+        for(n=i;n<=5;n++)
+        {
+            printf("%d\t",n);
+        }
+        printf("\n");
+    }
+    return(0);
+}
