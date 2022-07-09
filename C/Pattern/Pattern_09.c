c72753f2967df39729aee862d826c5e06575fa4c
Gunjan Kumar <gunjankumar0022@gmail.com>
2022-07-09 23:33:08 +0530

    Pattern_09.c Updated

--- C/Pattern/Pattern_09.c
+++ C/Pattern/Pattern_09.c
@@ -1,12 +1,20 @@
+/* Sample Output :- 1   
+                    2   3
+                    3   4   5
+                    4   5   6   7
+                    5   6   7   8   9
+*/
+
+
 #include <stdio.h>
 int main()
 {
-   int i,j,k=0;
+   int i,j,k;
    for(i=1;i<=5;i++)
    {
-    for(j=i;j<=i*5;j++)
+    for(j=0;j<i;j++)
     {
-        k = k + 1;
+        k = i + j;
         printf("%d\t",k);
     }
     printf("\n");
