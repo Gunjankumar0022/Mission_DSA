39d02c58afe1f245eac886f6169501ef325974cc
Gunjan Kumar <gunjankumar0022@gmail.com>
2022-07-10 23:36:08 +0530

    Factorial.c Added

---
+++ C/Recursion/Factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int factorial (int num)
+{
+    if (num == 0)
+        return(1);
+        return num * factorial(num - 1);
+}
+int main()
+{
+    int num,fact;
+    printf("Enter Number");
+    scanf("%d",&num);
+    if(num < 0)
+    {
+        printf("Error");
+    }
+    else
+    {
+        fact = factorial(num);
+        printf("%d",fact);
+    }
+    return(0);
+}
