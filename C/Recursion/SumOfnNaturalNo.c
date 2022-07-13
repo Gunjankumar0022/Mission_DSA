d2c8bad56536921b0baae7aef2a5ba73420fcd21
Gunjan Kumar <gunjankumar0022@gmail.com>
2022-07-13 22:15:05 +0530

    SumOfnNaturalNo.c Updated

--- C/Recursion/SumOfnNaturalNo.c
+++ C/Recursion/SumOfnNaturalNo.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
-int sum()
+int sum(int num)
+{
+    if(num == 1)
+    return 1; 
+    return num + sum(num - 1);
+}
+int main()
+{
+    int num;
+    printf("Enter Natural No :- ");
+    scanf("%d",&num);
+    printf("%d",sum(num));
+    return(0);
+}
